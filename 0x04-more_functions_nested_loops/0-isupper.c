2db7cb1c1e8e1fc963c87f166a70a2de341a6c30
amandawarima <amandawarima@gmail.com>
2023-10-15 12:26:56 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include 'main.h'
+
+/**
+ * _isupper - is the function of task
+ * @c: char to check
+ * Return: 0 or 1
+ */
+
+int _isupper(int c);
+
+{
+	int c;
+
+	if (c <= 'A' && c <= 'Z')
+	Return(1);
+	else
+	Return(0);
+}
