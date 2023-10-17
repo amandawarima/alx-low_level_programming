9fd7c86d2430fd3bc1218ef07f3c9cc38630dabe
amandawarima <amandawarima@gmail.com>
2023-10-17 08:09:33 -0700

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - function to swap two intergers
+ * @a: first parameter
+ * @b: second parameter
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c;
+c  = *b;
+*b = *a;
+*a = c;
+
+}
